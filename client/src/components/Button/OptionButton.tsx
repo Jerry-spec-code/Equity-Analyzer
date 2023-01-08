ac82370c7cd75c89c49e3118a20c8759c7ec4ac9
Jerry Li <jerry.limagic0302@gmail.com>
2023-01-08 13:05:26 -0700

    option form

--- client/src/components/Button/OptionButton.tsx
+++ client/src/components/Button/OptionButton.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { CircularProgress } from '@mui/material';
 
 type Props = {
-    setClicked : (args : boolean) => void
+    clicked : boolean;
+    setClicked : (args : boolean) => void;
+    updateStates : () => void;
 }
 
-const Btn = ({setClicked} : Props) => {
+const Btn = ({clicked, setClicked, updateStates} : Props) => {
   return (
     <div>   
-      <Button variant="contained" color="primary" sx={{textTransform: "None", width: "20%"}} onClick={() => setClicked(true)}>
+      <Button disabled={clicked} variant="contained" color="primary" sx={{textTransform: "None", width: "25%"}} onClick={() => {setClicked(true); updateStates();}}>
         <Typography sx={{ fontSize: "large"}}>
             Get call and put prices
         </Typography>
+        {clicked && <CircularProgress sx={{ ml : "15px"}}color="primary" />}
       </Button>
     </div>
   )
